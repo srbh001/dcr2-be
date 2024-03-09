68fd5c9c1eb9f638969840048ec00d971dddb6f5
“AdityaNeeraje” <aditya.neeraje@gmail.com>
2024-03-09 20:09:15 +0530

    Added corsheaders

--- base/settings.py
+++ base/settings.py
@@ -29,8 +29,16 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-4721.up.railway.app"
+]  # TODO in actual production
+ALLOWED_HOSTS = ["*"]  # TODO: Change this to host IP when deployinnfg
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000/"]
+
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -41,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
